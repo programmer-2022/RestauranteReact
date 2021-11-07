229ec958120434dc41b1c54ff70f577a90be0160
jonathan <jonathanguillermojm@ufps.edu.co>
2021-11-07 14:03:41 -0500

    contacto y reservas

---
+++ restaraunt-app/src/pages/reservas/Reservas.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import "./reservas.css"
+
+const Reservas = () => {
+	return (
+		<>
+			<div className="contenedor-titulo">
+        		<div className="div-titulo"><h1>Reservas</h1></div>
+    		</div> 
+
+		    <div className="container-fluid">
+		        <div className="container">              
+		            <div className="row contenedor-parrafos">
+		                <div className="col div-izq">
+		                    <p className="paragraph mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat, cupiditate deleniti eveniet voluptate non praesentium numquam laudantium eaque quidem. Harum unde, sed iste assumenda minima eligendi eveniet quam corrupti.</p>
+		                    <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat, cupiditate deleniti eveniet voluptate non praesentium numquam laudantium eaque quidem. Harum unde, sed iste assumenda minima eligendi eveniet quam corrupti.</p>
+		                </div>
+		                <div className="col">
+		                    <form>
+		                        	                        
+		                        <div className="mb-4">
+		                            <input type="text" className="form-control" placeholder="Nombre completo"/>
+		                        </div>
+
+		                        <div className="mb-4">
+		                            <input type="text" className="form-control" placeholder="Número de contacto"/>
+		                        </div>
+
+
+		                        <div className="mb-4">
+		                            <input type="email" className="form-control" placeholder="Correo Electrónico" required/>
+		                        </div>
+
+		                        <div className="mb-4">
+		                            <label htmlFor="cantidad">Cantidad de personas</label>
+		                            <input type="number" className="form-control" placeholder="Cantidad de personas" id="cantidad" required/>
+		                        </div>
+
+		                        <div className="mb-4">
+		                            <label htmlFor="fecha">Fecha de la reserva</label>
+		                            <input type="date" className="form-control" placeholder="Cantidad de personas" id="fecha" required/>
+		                        </div>
+
+		                        <div class="mb-4 my-select">
+		                            <select class="form-select" aria-label="Default select example">
+		                                <option selected>Seleccione el servicio</option>
+		                                <option value="1">Fiestas</option>
+		                                <option value="2">Cumpleaños</option>
+		                                <option value="3">Grado</option>
+		                            </select>
+		                            <span class="custom-arrow"></span>
+		                        </div>
+
+		                        <div className="mb-4">
+		                            <textarea className="form-control my-textarea" rows="4" placeholder="Su Mensaje"></textarea>
+		                        </div>
+
+		                        <div className="form-check mb-3">
+		                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+		                            <label className="form-check-label fst-italic mb-2" htmlFor="flexCheckDefault">
+		                            Acepto términos y condiciones
+		                            </label>
+		                        </div>
+		                        <div className="mb-3 text-center">
+		                            <button type="submit" className="btn mb-3">ENVIAR</button>
+		                        </div>
+		                    </form>
+		                </div>
+		            </div>   
+		        </div>
+		    </div>
+          
+		</>
+	)
+}
+
+export default Reservas
--- restaraunt-app/src/routes/Rutas.js
+++ restaraunt-app/src/routes/Rutas.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {Route, Routes} from "react-router-dom"
 
 import Home from "../pages/home/Home"
+import Contacto from "../pages/contacto/Contacto"
+import Reservas from "../pages/reservas/Reservas"
 
 const Rutas = () => {
     return (
         <Routes>
             <Route exact path="/" element={<Home/>} />
-            {/* <Route exact path="/menu" element={<Menu/>} /> */}
+            <Route exact path="/contacto" element={<Contacto/>} /> 
+            <Route exact path="/reservas" element={<Reservas/>} /> 
         </Routes>
     )
 }
