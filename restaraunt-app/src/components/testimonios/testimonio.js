ba22debc8fa3404e4fd1c386ce0bdf50ce403974
César Martínez <km.music92@gmail.com>
2021-11-07 13:02:19 -0500

    Se arregló sección de testimonios pagina home

--- restaraunt-app/src/components/testimonios/testimonio.js
+++ restaraunt-app/src/components/testimonios/testimonio.js
@@ -3,69 +3,26 @@ import '../testimonios/testimonio.css'
 
 export default function Testimonio() {
     return (
-        <section id="comentarios">
-            <div className="container">
-                <div className="content-center">
-                    <h1 className="padding-top padding-h1">Unas palabras de nuestros clientes</h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+        <div id="carouselExampleControls" className="carousel slide " data-bs-ride="carousel">
+            <div className="carousel-inner carouselBottom">
+                <div className="carousel-item active">
+                    <img src="assets/imagen.png" className="d-block w-100" alt="..." />
                 </div>
-                <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <p>Enim atque consectetur nesciunt cumque, iure esse voluptatem odit maxime,<br /> vel mollitia, earum doloremque veritatis pariatur inventore saepe temporibus. <br />Fuga, exercitationem architecto!</p>
-                            <div className="testimonial-user">
-                                <div className="row">
-                                    <div className="col-md-3">
-                                        <img src="assets/personal2@2x.png" alt="" className="img-fluid" />
-                                    </div>
-                                    <div className="col-md-9">
-                                        <h6>Sofia Mendez</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                            <div className="carousel-item">
-                                <p>Enim atque consectetur nesciunt cumque, iure esse voluptatem odit maxime,<br /> vel mollitia, earum doloremque veritatis pariatur inventore saepe temporibus. <br />Fuga, exercitationem architecto!</p>
-                                <div className="testimonial-user">
-                                    <div className="row">
-                                        <div className="col-md-3">
-                                            <img src="assets/personal1@2x.png" alt="" className="img-fluid" />
-                                        </div>
-                                        <div className="col-md-9">
-                                            <h6>Jairo Díaz</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        <div className="carousel-item">
-                            <p>Enim atque consectetur nesciunt cumque, iure esse voluptatem odit maxime,<br /> vel mollitia, earum doloremque veritatis pariatur inventore saepe temporibus. <br />Fuga, exercitationem architecto!</p>
-                            <div className="testimonial-user">
-                                <div className="row">
-                                    <div className="col-md-3">
-                                        <img src="assets/testimonio1@2x.png" alt="" className="img-fluid" />
-                                    </div>
-                                    <div className="col-md-9">
-                                        <h6>Sebastian Jimenez</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
+                <div className="carousel-item">
+                    <img src="assets/imagen2.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="assets/imagen3.png" className="d-block w-100" alt="..." />
                 </div>
             </div>
-        </section>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
     )
 }
