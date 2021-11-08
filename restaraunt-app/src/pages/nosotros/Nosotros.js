5c61f7bea93d8332c3331d9e569e4d3ced48df4e
César Martínez <km.music92@gmail.com>
2021-11-08 11:29:50 -0500

    Pagina nosotros en construccion, falta crear las tarjetas

---
+++ restaraunt-app/src/pages/nosotros/Nosotros.js
@@ -0,0 +1,107 @@
+import React from 'react'
+
+export default function Nosotros() {
+    return (
+        <div className="container mb-5"> 
+            <div className="container mt-4">
+                <h2 className="text-center">TESTIMONIOS</h2>
+            </div>
+
+            <hr/>
+
+            <div className="container mb-4">
+                <h5><span>4</span> COMENTARIOS</h5>
+            </div>
+
+            <div className="container p-1 position-relative">
+                
+                {/* Tarjeta para comentarios */}
+                <div className="card mb-3 border-0">
+                    <div className="row g-0">
+                        <div className="col-md-2 text-center">
+                            <img src="https://i.imgur.com/stD0Q19.jpg" className="rounded-circle" alt="..." style={{"width": "100px"}}/>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title d-inline me-2 align-mposition-relativeiddle">Luisa Maria</h5>
+                                <p className="card-text d-inline align-middle"><small className="text-muted">on Diciembre 12 2019 at 11:55 am</small></p>
+                                <p className="card-text mt-2">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <div className="position-absolute bottom-0 end-0">
+                                    <a href="#" class="btn btn-primary">Responder</a>
+                                </div>
+                            </div>                            
+                        </div>                        
+                    </div>
+                </div>
+
+                <div className="card mb-3 border-0">
+                    <div className="row g-0">
+                        <div className="col-md-2 text-center">
+                            <img src="https://i.imgur.com/xELPaag.jpg" className="rounded-circle" alt="..." style={{"width": "100px"}}/>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title d-inline me-2 align-mposition-relativeiddle">Valentina Rivas</h5>
+                                <p className="card-text d-inline align-middle"><small className="text-muted">on Diciembre 12 2019 at 11:55 am</small></p>
+                                <p className="card-text mt-2">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <div className="position-absolute bottom-0 end-0">
+                                    <a href="#" class="btn btn-primary">Responder</a>
+                                </div>
+                            </div>                            
+                        </div>                        
+                    </div>
+                </div>
+
+                <div className="card mb-3 border-0">
+                    <div className="row g-0">
+                        <div className="col-md-2 text-center">
+                            <img src="https://i.imgur.com/HjKTNkG.jpg" className="rounded-circle" alt="..." style={{"width": "100px"}}/>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title d-inline me-2 align-mposition-relativeiddle">Carlos Rendón</h5>
+                                <p className="card-text d-inline align-middle"><small className="text-muted">on Diciembre 12 2019 at 11:55 am</small></p>
+                                <p className="card-text mt-2">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <div className="position-absolute bottom-0 end-0">
+                                    <a href="#" class="btn btn-primary">Responder</a>
+                                </div>
+                            </div>                            
+                        </div>                        
+                    </div>
+                </div>
+
+                <div className="card mb-3 border-0">
+                    <div className="row g-0">
+                        <div className="col-md-2 text-center">
+                            <img src="https://i.imgur.com/nAcoHRf.jpg" className="rounded-circle" alt="..." style={{"width": "100px"}}/>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title d-inline me-2 align-mposition-relativeiddle">Esteban Martínez</h5>
+                                <p className="card-text d-inline align-middle"><small className="text-muted">on Diciembre 12 2019 at 11:55 am</small></p>
+                                <p className="card-text mt-2">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <div className="position-absolute bottom-0 end-0">
+                                    <a href="#" class="btn btn-primary">Responder</a>
+                                </div>
+                            </div>                            
+                        </div>                        
+                    </div>
+                </div>
+                
+            </div>
+
+            <div className="container py-2 mt-2">
+                <div className="container mb-4 p-0">
+                    <h5>Publicar</h5>
+                </div>            
+                <div className="mb-3">
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Escribe tu Comentario"></textarea>
+                </div>
+                <div className="container text-end p-0">
+                    <a href="#" className="btn btn-primary">Comentar</a>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- restaraunt-app/src/routes/Rutas.js
+++ restaraunt-app/src/routes/Rutas.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {Route, Routes} from "react-router-dom"
 
 import Home from "../pages/home/Home"
+import Nosotros from "../pages/nosotros/Nosotros"
 
 const Rutas = () => {
     return (
         <Routes>
             <Route exact path="/" element={<Home/>} />
-            {/* <Route exact path="/menu" element={<Menu/>} /> */}
+            <Route exact path="/nosotros" element={<Nosotros/>} />
         </Routes>
     )
 }
